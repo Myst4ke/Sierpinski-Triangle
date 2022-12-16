6cf8250ac707af0065da2b243cf67c60e5346fee
Florian <102389838+Myst4ke@users.noreply.github.com>
2022-12-16 17:33:26 +0100

    Sierpiński Triangle

---
+++ Sierpiński Triangle/js/main.js
@@ -0,0 +1,24 @@
+let cnv = document.getElementById("myCanvas");
+let ctx = cnv.getContext("2d");
+
+function drawSierpinskiTriangle(ctx, points, depth) {
+    // base case: if the depth is 0, draw the triangle
+    if (depth === 0) {
+      ctx.beginPath();
+      ctx.moveTo(points[0][0], points[0][1]);
+      ctx.lineTo(points[1][0], points[1][1]);
+      ctx.lineTo(points[2][0], points[2][1]);
+      ctx.fill();
+    } else {
+      // recursive case: draw three smaller triangles
+      drawSierpinskiTriangle(ctx, [points[0], getMidpoint(points[0], points[1]), getMidpoint(points[0], points[2])], depth - 1);
+      drawSierpinskiTriangle(ctx, [points[1], getMidpoint(points[0], points[1]), getMidpoint(points[1], points[2])], depth - 1);
+      drawSierpinskiTriangle(ctx, [points[2], getMidpoint(points[2], points[1]), getMidpoint(points[0], points[2])], depth - 1);
+    }
+  }
+  
+  function getMidpoint(point1, point2) {
+    return [(point1[0] + point2[0]) / 2, (point1[1] + point2[1]) / 2];
+  }
+  
+  drawSierpinskiTriangle(ctx, [[0, 0], [cnv.width, 0], [cnv.width/2, cnv.height]], 10);
